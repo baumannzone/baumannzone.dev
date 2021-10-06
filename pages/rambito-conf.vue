bb84b507c57e34a4819b3b5405d9b4512730359f
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2021-10-07 00:21:15 +0200

    feat: rambito conf header

--- pages/rambito-conf.vue
+++ pages/rambito-conf.vue
@@ -1,5 +1,160 @@
 <template>
-  <div>RambitoConf</div>
+  <div>
+    <div class="relative bg-gray-50 overflow-hidden">
+      <div
+        class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
+        aria-hidden="true"
+      >
+        <div class="relative h-full max-w-7xl mx-auto">
+          <svg
+            class="
+              absolute
+              right-full
+              transform
+              translate-y-1/4 translate-x-1/4
+              lg:translate-x-1/2
+            "
+            width="404"
+            height="784"
+            fill="none"
+            viewBox="0 0 404 784"
+          >
+            <defs>
+              <pattern
+                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  class="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="784"
+              fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+            />
+          </svg>
+          <svg
+            class="
+              absolute
+              left-full
+              transform
+              -translate-y-3/4 -translate-x-1/4
+              md:-translate-y-1/2
+              lg:-translate-x-1/2
+            "
+            width="404"
+            height="784"
+            fill="none"
+            viewBox="0 0 404 784"
+          >
+            <defs>
+              <pattern
+                id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  class="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="784"
+              fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div class="relative pt-6 pb-16 sm:pb-24">
+        <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+          <div class="text-center">
+            <h1
+              class="
+                text-4xl
+                tracking-tight
+                font-extrabold
+                text-gray-900
+                sm:text-5xl
+                md:text-6xl
+              "
+            >
+              <span class="block mb-2">Rambito Conf</span>
+              <span class="block text-purple-600">Hacktoberfest Edition</span>
+            </h1>
+            <p
+              class="
+                my-5
+                max-w-md
+                mx-auto
+                text-xl text-gray-500
+                sm:text-lg
+                md:mt-5 md:text-xl md:max-w-3xl
+              "
+            >
+              Lunes 11 de Octubre. De 18:00 a 20:00 en Twitch.
+            </p>
+            <div class="mt-16 mx-auto sm:flex sm:justify-center md:mt-8">
+              <a
+                href="twitch.tv/baumannzone"
+                class="
+                  px-8
+                  py-3
+                  border border-transparent
+                  text-base
+                  font-medium
+                  rounded-md
+                  text-white
+                  bg-purple-600
+                  hover:bg-purple-700
+                "
+              >
+                twitch.tv/baumannzone
+              </a>
+              <!-- <a
+                  href="#"
+                  class="
+                    w-full
+                    px-8
+                    py-3
+                    border border-transparent
+                    text-base
+                    font-medium
+                    rounded-md
+                    text-purple-600
+                    bg-white
+                    hover:bg-gray-50
+                    md:py-4 md:text-lg md:px-10
+                  "
+                >
+                  Live demo
+                </a> -->
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
