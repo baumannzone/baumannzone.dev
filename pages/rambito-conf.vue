0453dc8a75e0fa01f4a34c821e06fd5c636c417f
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2021-10-08 18:39:34 +0200

    feat: small fixes and improvements

--- pages/rambito-conf.vue
+++ pages/rambito-conf.vue
@@ -121,7 +121,7 @@
                 md:mt-5 md:text-xl md:max-w-3xl
               "
             >
-              Lunes 11 de Octubre. De 18:00 a 20:00 en Twitch.
+              Lunes 11 de Octubre. De 18:00 a 20:30 en Twitch.
             </p>
             <div class="mt-16 mx-auto sm:flex sm:justify-center md:mt-8">
               <a
@@ -180,46 +180,32 @@
                   <img
                     class="object-cover shadow-lg rounded-lg"
                     :src="user.image"
-                    :alt="`Imagen de ${user.name}`"
+                    :alt="`Imagen de ${user.displayName}`"
                   />
                 </div>
 
                 <div class="space-y-2">
                   <div class="text-lg leading-6 font-medium space-y-1">
-                    <h3>{{ user.name }}</h3>
+                    <h3>{{ user.displayName }}</h3>
                     <p class="text-purple-600">{{ user.role }}</p>
                   </div>
                   <ul role="list" class="flex space-x-5">
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-500">
-                        <span class="sr-only">Twitter</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="text-gray-400 hover:text-gray-500">
-                        <span class="sr-only">LinkedIn</span>
-                        <svg
-                          class="w-5 h-5"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          aria-hidden="true"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
+                    <li
+                      v-for="socialItem in user.social"
+                      :key="socialItem.name"
+                    >
+                      <a
+                        :href="socialItem.url"
+                        class="text-gray-400 hover:text-gray-500"
+                        target="_blank"
+                        rel="noopener"
+                      >
+                        <span class="sr-only">{{ socialItem.name }}</span>
+                        <SvgIcon
+                          :icon-name="socialItem.name"
+                          width="25"
+                          height="25"
+                        />
                       </a>
                     </li>
                   </ul>
@@ -525,7 +511,11 @@
             </div>
           </div>
           <div class="mt-8 lg:mt-0 grid grid-cols-1 gap-0.5">
-            <a href="https://vercel.com?ref=baumannzone.dev" target="_blank">
+            <a
+              href="https://vercel.com?ref=baumannzone.dev"
+              target="_blank"
+              rel="noopener"
+            >
               <div
                 class="
                   col-span-1
@@ -551,7 +541,7 @@
               </div>
             </a>
 
-            <a href="https://bit.ly/2WN0Nf4" target="_blank">
+            <a href="https://bit.ly/2WN0Nf4" target="_blank" rel="noopener">
               <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
                 <svg
                   id="Capa_1"
@@ -831,7 +821,11 @@
               </div>
             </a>
 
-            <a href="https://codely.tv/pro/rambitoconf" target="_blank">
+            <a
+              href="https://codely.tv/pro/rambitoconf"
+              target="_blank"
+              rel="noopener"
+            >
               <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
                 <img
                   class="max-h-12"
@@ -849,8 +843,13 @@
 </template>
 
 <script>
+import SvgIcon from '@/components/SvgIcon'
+
 export default {
   name: 'RambitoConfPage',
+  components: {
+    SvgIcon,
+  },
   data() {
     return {
       agenda: [
@@ -886,22 +885,49 @@ export default {
       ],
       team: [
         {
-          name: 'Rambito JS',
+          displayName: 'Rambito JS',
           role: 'CEO',
           image: '/rambito-conf/rambito.jpg',
-          social: [],
+          social: [
+            {
+              name: 'youtube',
+              url: 'https://www.youtube.com/rambitoJS',
+            },
+          ],
         },
         {
-          name: 'Jorge Baumann',
+          displayName: 'Jorge Baumann',
           role: 'Full Stack JavaScript Developer',
           image: '/rambito-conf/baumann.jpg',
-          social: [],
+          social: [
+            {
+              name: 'twitter',
+              url: 'https://twitter.com/baumannzone',
+            },
+            {
+              name: 'instagram',
+              url: 'https://www.instagram.com/baumannzone',
+            },
+            {
+              name: 'twitch',
+              url: 'https://www.twitch.tv/baumannzone',
+            },
+          ],
         },
         {
-          name: 'Ari',
+          displayName: 'Ari',
           role: 'Reinventada',
           image: '/rambito-conf/ari.jpg',
-          social: [],
+          social: [
+            {
+              name: 'twitter',
+              url: 'https://twitter.com/ari_reinventada',
+            },
+            {
+              name: 'twitch',
+              url: 'https://www.twitch.tv/ari_reinventada',
+            },
+          ],
         },
       ],
     }
