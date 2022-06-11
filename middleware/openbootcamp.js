f7abc30769fd9350ebe045f609b63b9619724616
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2022-06-11 10:27:59 +0200

    feat: open bootcamp

---
+++ middleware/openbootcamp.js
@@ -0,0 +1,5 @@
+export default ({ redirect }) => {
+  redirect(
+    'https://open-bootcamp.com/?campanya=1&grupo=twitter&palabraClave=baumannzone&source=twitter&medium=referer&campaign=twitter'
+  )
+}
