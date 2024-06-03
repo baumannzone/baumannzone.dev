a0f958c951c9300792653f7d9485614b0e4a8c6e
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2024-06-04 00:46:54 +0200

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,12 +5,13 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+    customData: `<language>es</language>`,
+  });
 }
