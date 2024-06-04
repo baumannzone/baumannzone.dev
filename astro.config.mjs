a7882b26723888985c0c7f947d60cb2c3ccae547
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2024-06-04 23:16:55 +0200

    update nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
